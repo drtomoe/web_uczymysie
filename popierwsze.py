f1243c654da44646be18384c9bdaab8a34304bea
Paulina Lorek <inztomoe@gmail.com>
2018-08-22 12:34:36 +0200

    web2

--- popierwsze.py
+++ popierwsze.py
@@ -1,18 +1,33 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
+posts = [
+    {
+        "autor": "ina",
+        "title": "post 1",
+        "content": "first post content",
+        "date": "21.08.2018"
+    },
+    {
+        "autor": "tomoe",
+        "title": "post 2",
+        "content": "second post content",
+        "date": "22.08.2018"
+    }
+
+]
 
 @app.route("/")
 @app.route("/home")
 
 def hello():
-    return "<h1>main page</h1>"
+    return render_template("home.html", posts=posts)
 
 
 @app.route("/about")
 def about():
-    return "<h1>about page</h1>"
+    return render_template("about.html", title="About")
 
 
 if __name__ == "__main__":
