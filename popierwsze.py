7d538c4d4a95764b81a79cbb37f6d5fd7cd188da
Paulina Lorek <inztomoe@gmail.com>
2018-08-24 13:49:52 +0200

    web4

--- popierwsze.py
+++ popierwsze.py
@@ -1,10 +1,40 @@
+from datetime import datetime
 from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
 
-app = Flask(__name__)
 
+#pliki konfiguracyjne
+
+app = Flask(__name__)
 app.config["SECRET_KEY"] = "9fa4a357a8e67403dc5631d1806f9029"
-#konsola python --> import secrets --> secrets.token_hex(16)   --> to daje losowy ciąg 16 znaków
+    #konsola python --> import secrets --> secrets.token_hex(16) > to daje losowy ciąg 16 znaków potrzebny do bezpieczeństwa
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///site.db' #adres /// oznacza, ze w db będzie w tym samym miejscu, co reszta plików
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+    post = db.relationship('Post', backref='author', lazy=True)  #backref - dodaje kolumnę do modułu Post - nazwaną "author"
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False) #user.id z małej bo to odniesienie do wiersza tabeli User a nie do całej klasy
+
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.date_posted}')"
+
+
+
 
 posts = [
     {
@@ -24,7 +54,6 @@ posts = [
 
 @app.route("/")
 @app.route("/home")
-
 def home():
     return render_template("home.html", posts=posts)
     #zwróć wygenerowaną stronę -->home, przekaż posty
